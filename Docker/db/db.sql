6a2fc18ff72484853a0494ec92cab5e4c0a1c9f0
Gennady Zolotukhin <g.a.zolotukhin@gmail.com>
2021-12-12 11:37:41 +0300

    For starting Laravel and Yii2 project

---
+++ Docker/db/db.sql
@@ -0,0 +1,12 @@
+-- Adminer 4.7.7 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+
+SET NAMES utf8mb4;
+
+CREATE DATABASE `local_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `local_database`;
+
+-- 2020-12-18 06:09:52
